c41450ef0dfd05564f4fe56370d952117b11fc8c
Gavin Nimsara Gunapala <68413021+gavingunapala@users.noreply.github.com>
2022-02-19 09:10:09 +0530

    first commit

---
+++ poc/src/js/controler.js
@@ -0,0 +1,119 @@
+// 'use strict';
+
+// const form = document.querySelector('.form');
+// const btn = document.querySelector('.form__btn');
+// const inputname = document.querySelector('.form__input--name');
+// const inputDate = document.querySelector('.form__input--Date');
+// const inputTitle = document.querySelector('.form__input--Title');
+// const inputTodo = document.querySelector('.form__input--Todo');
+
+// //CLASS
+// class List {
+//   constructor(name, date, title, todolist) {
+//     this.name = name;
+//     this.date = date;
+//     this.title = title;
+//     this.todolist = todolist;
+//   }
+// }
+
+// // const list = new List('aa', 'aaa', 'aaa', 'aaa');
+// // console.log(list);
+
+// //APPLICATION
+// class App {
+//   #ToDoList = [];
+//   constructor() {
+//     form.addEventListener('submit', this._getData.bind(this));
+//   }
+
+//   _getData = function (e) {
+//     e.preventDefault();
+//     const name = inputname.value;
+//     const date = inputDate.value;
+//     const title = inputTitle.value;
+//     const todo = inputTodo.value;
+//     const list = new List(name, date, title, todo);
+//     console.log(list);
+//     this.#ToDoList.push(list);
+//     this._renderWorkout(list);
+//     this._cleardata();
+//   };
+//   _cleardata() {
+//     inputname.value = inputDate.value = inputTitle.value = inputTodo.value = '';
+//   }
+
+//   _renderWorkout(workout) {
+//     let html = `
+//       <li class="workout workout--${workout.name}" data-id="${workout.date}">
+//         <h2 class="workout__title">${workout.title}</h2>
+//         <div class="workout__details">
+//           <span class="workout__value">${workout.todo}</span>
+//           <span class="workout__unit">km</span>
+//         </div>
+//     `;
+//     form.insertAdjacentHTML('afterend', html);
+//   }
+// }
+
+// const app = new App();
+
+'use strict';
+
+const form = document.querySelector('.form');
+const btn = document.querySelector('.form__btn');
+const inputname = document.querySelector('.form__input--name');
+const inputDate = document.querySelector('.form__input--Date');
+const inputTitle = document.querySelector('.form__input--Title');
+const inputTodo = document.querySelector('.form__input--Todo');
+
+//CLASS
+class List {
+  constructor(name, date, title, todolist) {
+    this.name = name;
+    this.date = date;
+    this.title = title;
+    this.todolist = todolist;
+  }
+}
+
+// const list = new List('aa', 'aaa', 'aaa', 'aaa');
+// console.log(list);
+
+//APPLICATION
+class App {
+  #ToDoList = [];
+  constructor() {
+    form.addEventListener('submit', this._getData.bind(this));
+  }
+
+  _getData = function (e) {
+    e.preventDefault();
+    const name = inputname.value;
+    const date = inputDate.value;
+    const title = inputTitle.value;
+    const todo = inputTodo.value;
+    const list = new List(name, date, title, todo);
+    console.log(list);
+    this.#ToDoList.push(list);
+    this._renderWorkout(list);
+    this._cleardata();
+  };
+  _cleardata() {
+    inputname.value = inputDate.value = inputTitle.value = inputTodo.value = '';
+  }
+
+  _renderWorkout(workout) {
+    let html = `
+      <li class="workout workout--${workout.name}" data-id="${workout.date}">
+        <h2 class="workout__title">${workout.title}</h2>
+        <div class="workout__details">
+          <span class="workout__value">${workout.todo}</span>
+          <span class="workout__unit">km</span>
+        </div>
+    `;
+    form.insertAdjacentHTML('afterend', html);
+  }
+}
+
+const app = new App();
---
+++ poc/src/js/view/todoView.js
@@ -0,0 +1,27 @@
+// 'use strict';
+
+// class todoView {
+//   form = document.querySelector('.form');
+//   btn = document.querySelector('.form__btn');
+//   inputname = document.querySelector('.form__input--name');
+//   inputDate = document.querySelector('.form__input--Date');
+//   inputTitle = document.querySelector('.form__input--Title');
+//   inputTodo = document.querySelector('.form__input--Todo');
+//   constructor() {}
+
+//   //private methord to get data from form
+//   _newData(e) {
+//     e.preventDefault();
+//     const name = this.inputname.value;
+//     const date = this.inputDate.value;
+//     const title = this.inputTitle.value;
+//     const todo = this.inputTodo.value;
+//     console.log(name, date, title, todo);
+//   }
+
+//   addHanddlerAddData() {
+//     this.form.addEventListener('submit', this._newData.bind(this));
+//     handler();
+//   }
+// }
+// export default new todoView();
